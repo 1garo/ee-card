2dd38af2e642b08bc1c0ed936b667acd05965f0b
1garo <alevardai@hotmail.com>
2020-09-11 18:47:06 -0300

    db configs

---
+++ src/configs/database.ts
@@ -0,0 +1,11 @@
+export default {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 5433,
+  define: {
+    timestamps: true,
+  },
+};
