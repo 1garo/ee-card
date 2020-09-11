4a15000a6c8acf19f5e64daab8cf615942471e68
1garo <alevardai@hotmail.com>
2020-09-11 18:46:44 -0300

    testing model creation

--- src/app/window.ts
+++ src/app/window.ts
@@ -1,5 +1,6 @@
 import { BrowserWindow } from 'electron';
-
+import Employee from "../model/employee.model";
+import { Sequelize } from 'sequelize';
 export function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -10,7 +11,17 @@ export function createWindow () {
     },
     backgroundColor: 'grey'
   })
-
+  const test = new Employee();
+  test.sequelize.sync({force: true}).then(a => console.log(a)).catch((err) => console.log(err));
+  // const sequelize = new Sequelize('postgres://alevardai:dev_db@localhost:5432/ee_card');
+  // sequelize
+  // .authenticate()
+  // .then(() => {
+  //   console.log('Connection has been established successfully.');
+  // })
+  // .catch(err => {
+  //   console.error('Unable to connect to the database:', err);
+  // });
   // and load the index.html of the app.
   win.loadFile('../index.html')
 
