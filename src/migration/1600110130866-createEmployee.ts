0916c10f21a92e3d309a3455ee750a2601df4490
1garo <alevardai@hotmail.com>
2020-09-15 17:33:34 -0300

    add new tables and realtions to employee migration

--- src/migration/1600110130866-createEmployee.ts
+++ src/migration/1600110130866-createEmployee.ts
@@ -96,28 +96,82 @@ export default class createEmployee1600110130866 implements MigrationInterface {
             ]
         }), true);
 
+        await queryRunner.createTable(new Table({
+            name: 'card',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                    isPrimary: true,
+                },
+                {
+                    name: 'cardNumber',
+                    type: 'varchar', 
+                    isNullable: false
+                },
+                {
+                    name: 'expirationDate',
+                    type: 'varchar', 
+                    isNullable: false
+                },
+                {
+                    name: 'securityCode',
+                    type: 'int', 
+                    isNullable: false
+                },
+                {
+                    name: 'createdAt',
+                    type: 'timestamp', 
+                    default: 'now()',
+                    isNullable: false
+                },
+                {
+                    name: 'updatedAt',
+                    type: 'timestamp', 
+                    default: 'now()',
+                    isNullable: false
+                },
+            ]
+        }), true);
+
         await queryRunner.addColumn("employee", new TableColumn({
             name: "companyId",
             type: "uuid"
         }));
 
-        await queryRunner.createForeignKey("employee", new TableForeignKey({
+        await queryRunner.addColumn("employee", new TableColumn({
+            name: "ecardId",
+            type: "uuid"
+        }));
+
+        await queryRunner.createForeignKeys("employee", [new TableForeignKey({
             columnNames: ["companyId"],
             referencedColumnNames: ["id"],
             referencedTableName: "company",
             onDelete: "CASCADE"
-        }));
+        }), new TableForeignKey({
+            columnNames: ["ecardId"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "card",
+            onDelete: "CASCADE"
+        })]);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         const table = await queryRunner.getTable("employee");
         if (table != undefined) {
-            const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf("companyId") !== -1);
-            if (foreignKey != undefined) {
-            await queryRunner.dropForeignKey("employee", foreignKey);
+            const fkCompany = table.foreignKeys.find(fk => fk.columnNames.indexOf("companyId") !== -1);
+            const fkCard = table.foreignKeys.find(fk => fk.columnNames.indexOf("ecardId") !== -1);
+            if (fkCard != undefined && fkCompany != undefined) {
+            await queryRunner.dropForeignKey("employee", fkCompany);
             await queryRunner.dropColumn("employee", "companyId");
+            await queryRunner.dropForeignKey("employee", fkCard);
+            await queryRunner.dropColumn("employee", "ecardId");
             await queryRunner.dropTable("employee");
             await queryRunner.dropTable("company");
+            await queryRunner.dropTable("card");
             }
         }
     }
