2bb0e877b9ef0c5433921e33052942c69c169d92
1garo <alevardai@hotmail.com>
2020-09-16 17:51:07 -0300

    use new route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { PORT } from '../config.json';
+import { employeeRouter } from './router/employeeRoute';
 const app = express();
 app.use(json());
+app.use(employeeRouter);
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
