257c920ebdb7713cc890f0dc227af4f4cc2a0102
1garo <alevardai@hotmail.com>
2020-09-23 15:57:41 -0300

    use new card router

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { json } from 'body-parser';
 import { PORT } from '../config.json';
 import { employeeRouter } from './router/employeeRoute';
+import { cardRouter } from './router/cardRoute';
 const app = express();
 app.use(json());
 app.use(employeeRouter);
+app.use(cardRouter);
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
