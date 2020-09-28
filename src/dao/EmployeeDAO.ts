c88e9b7f03f61b4618fabdfe95aa0c54209a7f41
1garo <alevardai@hotmail.com>
2020-09-28 16:42:07 -0300

    remove debug log

--- src/dao/EmployeeDAO.ts
+++ src/dao/EmployeeDAO.ts
@@ -4,8 +4,6 @@ import { Card } from "../entity/Card";
 import { Request } from "express";
 
 export async function findEmployeeByPassword(employee: Employee, CONN: Promise<Connection>): Promise<Employee | undefined> {
-  console.log(employee);
-
   return await (await CONN).getRepository(Employee)
   .createQueryBuilder("employee")
   .where("employee.passwordHash = :hash", { hash: employee.passwordHash })
