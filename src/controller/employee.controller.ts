c6d138f0b9ce272bbd6dd1dd8eee3ba418cd21e9
1garo <alevardai@hotmail.com>
2020-09-28 16:41:30 -0300

    solve new employee insert problem

--- src/controller/employee.controller.ts
+++ src/controller/employee.controller.ts
@@ -11,12 +11,15 @@ export async function getEmployeeByHash(password: string, CONN: Promise<Connecti
 
 export async function setEmployee(body: any, basicAuth: string, CONN: Promise<Connection>) {
   let newCard = new Card();
-  const user = new Employee();
+  let user = new Employee();
   newCard = body.card;
   delete body['card'];
   const companyName = body.companyName;
   delete body['companyName']
-  user.age = body;
+  user.age = body.age;
+  user.email = body.email;
+  user.firstName = body.firstName;
+  user.lastName = body.lastName;
   const auth = Buffer.alloc(basicAuth.length, basicAuth, 'base64').toString();
   await user.encrypt(user, auth)
   return await employeeService.create(user, newCard, companyName, CONN); 
