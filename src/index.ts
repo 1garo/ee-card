306374819a1d81ffdb9c2356a50c0b2cff8e5370
1garo <alevardai@hotmail.com>
2020-09-28 16:40:14 -0300

    add new company route

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { json } from 'body-parser';
 import { PORT } from '../config.json';
 import { employeeRouter } from './router/employeeRoute';
 import { cardRouter } from './router/cardRoute';
+import { companyRouter } from './router/companyRoute';
 const app = express();
 app.use(json());
 app.use(employeeRouter);
 app.use(cardRouter);
+app.use(companyRouter);
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
