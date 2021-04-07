1909a385413082bad5f1c4288dd5462dd64f1d07
1garo <alevardai@hotmail.com>
2021-04-07 16:08:33 -0300

    feat: add walk through of k8s deployment

--- db.sh
+++ db.sh
@@ -7,3 +7,10 @@ create user ee_card_user with encrypted password 'ee_card_password';
 grant all privileges on database ee_card to ee_card_user;
 
 EOF
+
+minikube start --driver=docker
+minikube kubectl -- create -f ./k8s/server-deployment.yaml
+minikube kubectl -- create -f ./k8s/database-deployment.yaml
+minikube kubectl -- expose deployment database --type=NodePort --name=database 
+minikube kubectl -- expose deployment server --type=NodePort --name=ee-card-server --port=8000
+minikube service list # get the ee-card-server url and use that as url (e.g GET url/card)
