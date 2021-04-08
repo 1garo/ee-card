c93e90861fbe0318566ae86c30e3f29396c1e849
1garo <alevardai@hotmail.com>
2021-04-07 23:12:48 -0300

    feat: fix typo on unused params

--- src/router/v1/card.ts
+++ src/router/v1/card.ts
@@ -2,7 +2,8 @@ import * as fast from 'fastify'
 import {CONN} from "../../database/db";
 import * as cardController from '../../controller/card.controller';
 
-const card = async (fastify, opts, done) => {
+const card = async (fastify, _opts, done) => {
+
   fastify.get('/card/number/:cardNumber', async (req, reply: fast.FastifyReply) => {
     const cardNumber = req.params.cardNumber;
     if (cardNumber === '') {
@@ -13,16 +14,16 @@ const card = async (fastify, opts, done) => {
       return reply.code(404).send({error: "Cardnumber wasn't found!"});
     }
     return reply.code(200).send(resp);
-
-    await reply.send({ hello: 'cards' });
   });
-  fastify.get('/card', async (req, reply: fast.FastifyReply) => {
+
+  fastify.get('/card', async (_req, reply: fast.FastifyReply) => {
     const cards = await cardController.getAllCards(CONN);
     if (cards === undefined){
       return reply.code(404).send(cards);
     }
     return reply.code(200).send({cards});
   });
+
   fastify.post('/card', async (req, reply: fast.FastifyReply) => {
     const {message, status} =  await cardController.setCard(req.body, CONN);
     if (status === 500){
@@ -30,6 +31,7 @@ const card = async (fastify, opts, done) => {
     }
     return reply.code(status).send({message: `${message}`})
   });
+
   done();
 };
 
