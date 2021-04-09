bf1cbc7c08a6f1130a27f6184e38a322cfd0eff8
1garo <alevardai@hotmail.com>
2021-04-08 21:43:28 -0300

    feat: remove useless comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import express from 'express';
-import { json } from 'body-parser';
 import fastify from 'fastify';
-//import { employeeRouter } from './router/employeeRoute';
-//import { cardRouter } from './router/cardRoute';
-//import { companyRouter } from './router/companyRoute';
 import { cardRouter } from './router/v1/card';
 import { companyRouter } from './router/v1/company';
 import { employeeRouter } from './router/v1/employee';
-//import {} from './router/v1/users';
-//const app = express();
+
 //TODO: generate kompose again because a typo on db user name
 const app = fastify(
   {logger: true}
