a9c81c80b973877fe2ecd48032a0ea2b2d1cfa81
1garo <alevardai@hotmail.com>
2021-04-08 21:44:38 -0300

    feat: unfinished employee seed code

--- src/migration/1600449956251-SeedDB.ts
+++ src/migration/1600449956251-SeedDB.ts
@@ -1,10 +1,14 @@
-import {MigrationInterface, QueryRunner, getRepository, getConnection} from "typeorm";
+import {MigrationInterface, QueryRunner } from "typeorm";
 import { CardSeed } from "../seed/card.seed";
 import { CompanySeed } from "../seed/company.seed";
+import { EmployeeSeed } from "../seed/employee.seed";
 import { Company } from "../entity/Company";
+import { Employee } from "../entity/Employee";
 import { Card } from "../entity/Card";
 import { hash } from "bcryptjs";
-
+import { getAllCards } from "../controller/card.controller";
+import { getAllCompanies } from "../controller/company.controller";
+import { CONN } from "../database/db";
 /**
  * yarn build && yarn run typeorm schema:sync
  * yarn build && yarn run typeorm schema:drop
@@ -21,7 +25,7 @@ export class SeedDB1600449956251 implements MigrationInterface {
             .values(value)
             .execute();
         });
-        const h = await hash('admin', 8);
+        let h = await hash('admin', 8);
         CompanySeed.forEach(async value => {
             value.passwordHash = h; 
             await queryRunner.connection.createQueryBuilder()
@@ -30,6 +34,19 @@ export class SeedDB1600449956251 implements MigrationInterface {
             .values(value)
             .execute();
         }); 
+        // h = await hash('employee', 8);
+        // //TODO: implement a for each company and card to seed new employees on db
+        // // e.g getcompanybyID
+        // EmployeeSeed.forEach(async value => {
+        //     await getAllCompanies(CONN)
+        //     await getAllCards(CONN)
+        //     value.passwordHash = h; 
+        //     await queryRunner.connection.createQueryBuilder()
+        //     .insert()
+        //     .into(Employee)
+        //     .values(value)
+        //     .execute();
+        // }); 
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
