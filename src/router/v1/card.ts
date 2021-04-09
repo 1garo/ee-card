b4ea521e894711452e7fbd75341752ff520e5d8f
1garo <alevardai@hotmail.com>
2021-04-08 21:48:35 -0300

    feat: remove useless return statement

--- src/router/v1/card.ts
+++ src/router/v1/card.ts
@@ -4,34 +4,36 @@ import * as cardController from '../../controller/card.controller';
 
 const card = async (fastify, _opts, done) => {
 
-  fastify.get('/card/number/:cardNumber', async (req, reply: fast.FastifyReply) => {
-    const cardNumber = req.params.cardNumber;
-    if (cardNumber === '') {
-      return reply.code(400).send({error: "cardNumber as parameter wasn't provided!"})
-    }
-    const resp = await cardController.getCardByNumber(cardNumber, CONN);
-    if (resp === undefined){
-      return reply.code(404).send({error: "Cardnumber wasn't found!"});
-    }
-    return reply.code(200).send(resp);
-  });
+  fastify.get('/card/number/:cardNumber', async function (req, reply: fast.FastifyReply) {
+      const cardNumber = req.params.cardNumber;
+      if (cardNumber === '') {
+        reply.code(400).send({ error: "cardNumber as parameter wasn't provided!" });
+      }
+      const resp = await cardController.getCardByNumber(cardNumber, CONN);
+      if (resp === undefined) {
+        reply.code(404).send({ error: "Cardnumber wasn't found!" });
+      }
 
-  fastify.get('/card', async (_req, reply: fast.FastifyReply) => {
+      reply.code(200).send(resp);
+    });
+
+  fastify.get('/card', async function (reply: fast.FastifyReply) {
     const cards = await cardController.getAllCards(CONN);
     if (cards === undefined){
-      return reply.code(404).send(cards);
+       reply.code(404).send(cards);
     }
+
     reply.code(200).send(cards)
-    // return reply.code(200).send({cards});
   });
 
-  fastify.post('/card', async (req, reply: fast.FastifyReply) => {
-    const {message, status} =  await cardController.setCard(req.body, CONN);
-    if (status === 500){
-      return reply.code(status).send({error: `${message}`})
-    }
-    return reply.code(status).send({message: `${message}`})
-  });
+  fastify.post('/card', async function (req, reply: fast.FastifyReply) {
+      const { message, status } = await cardController.setCard(req.body, CONN);
+      if (status === 500) {
+        reply.code(status).send({ error: `${message}` });
+      }
+
+      reply.code(status).send({ message: `${message}` });
+    });
 
   done();
 };
