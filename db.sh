77ac7e5a83a0665c8ca5b809f20acd79f6a753fc
1garo <alevardai@hotmail.com>
2021-04-23 18:23:10 -0300

    feat: add LoadBalancer way to deploy using k8s

--- db.sh
+++ db.sh
@@ -8,6 +8,8 @@ grant all privileges on database ee_card to ee_card_user;
 
 EOF
 
+
+# way to expose without using service yaml and use NodePort 
 minikube start --driver=docker
 minikube docker-env
 eval $(minikube -p minikube docker-env)
@@ -17,5 +19,14 @@ minikube kubectl -- create -f ./k8s/database-deployment.yaml
 minikube kubectl -- expose deployment database --type=NodePort --name=database
 # delay some seconds to create the server, due to sync and migration
 minikube kubectl -- create -f ./k8s/server-deployment.yaml
-minikube kubectl -- expose deployment server --type=NodePort --name=ee-card-server --port=8000
+minikube kubectl -- expose deployment server --type=NodePort --name=server --port=8000
 minikube service list # get the ee-card-server url and use that as url (e.g GET url/card)
+
+# way to expose without using service yaml and use LoadBalancer
+minikube start 
+minikube kubectl -- apply -f ./k8s/database-data-persistentvolumeclaim.yaml
+minikube kubectl -- apply -f ./k8s/database-deployment.yaml
+minikube kubectl -- expose deployment database --type=LoadBalancer --name=database
+
+minikube kubectl -- apply -f ./k8s/server-deployment.yaml
+minikube kubectl -- expose deployment server --type=LoadBalancer --name=server --port=8000
